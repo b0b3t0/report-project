cd10bc9a26359276de8399e45839c75b7ee4142a
b0b3t0 <bnenov91@gmail.com>
2021-05-07 16:30:58 +0300

    Created class to save the name and scores of every desirialized seller

---
+++ src/main/java/data/output/SellerResult.java
@@ -0,0 +1,28 @@
+package data.output;
+
+public class SellerResult {
+
+    private final String name;
+    private final double score;
+
+    public SellerResult(String name, double score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return "SellerResult{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}
