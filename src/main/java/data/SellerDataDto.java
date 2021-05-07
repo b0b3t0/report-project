113aec87e94e6b8737d9ae89e6e452a3ea1b17df
b0b3t0 <bnenov91@gmail.com>
2021-05-07 15:55:58 +0300

    Added dto class for deserializing from json to java object

---
+++ src/main/java/data/SellerDataDto.java
@@ -0,0 +1,43 @@
+package data;
+
+public class SellerDataDto {
+
+    private final String name;
+    private final int totalSales;
+    private final int salesPeriod;
+    private final double experienceMultiplier;
+
+
+    public SellerDataDto(String name, int totalSales, int salesPeriod, double experienceMultiplier) {
+        this.name = name;
+        this.totalSales = totalSales;
+        this.salesPeriod = salesPeriod;
+        this.experienceMultiplier = experienceMultiplier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalSales() {
+        return totalSales;
+    }
+
+    public int getSalesPeriod() {
+        return salesPeriod;
+    }
+
+    public double getExperienceMultiplier() {
+        return experienceMultiplier;
+    }
+
+    @Override
+    public String toString() {
+        return "SellerDataDto{" +
+                "name='" + name + '\'' +
+                ", totalSales=" + totalSales +
+                ", salesPeriod=" + salesPeriod +
+                ", experienceMultiplier=" + experienceMultiplier +
+                '}';
+    }
+}
