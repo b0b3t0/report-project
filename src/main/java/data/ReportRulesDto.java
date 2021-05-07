e45ff0c89b0ec9185c1f8091c076ca89f6d340f3
b0b3t0 <bnenov91@gmail.com>
2021-05-07 16:03:59 +0300

    Added dto class for deserializing from json to java object rules for calculating the score of sellers

---
+++ src/main/java/data/ReportRulesDto.java
@@ -0,0 +1,36 @@
+package data;
+
+public class ReportRulesDto {
+
+    private final int topPerformersThreshold;
+    private final boolean useExperienceMultiplier;
+    private final int periodLimit;
+
+
+    public ReportRulesDto(int topPerformersThreshold, boolean useExperienceMultiplier, int periodLimit) {
+        this.topPerformersThreshold = topPerformersThreshold;
+        this.useExperienceMultiplier = useExperienceMultiplier;
+        this.periodLimit = periodLimit;
+    }
+
+    public int getTopPerformersThreshold() {
+        return topPerformersThreshold;
+    }
+
+    public boolean isUseExperienceMultiplier() {
+        return useExperienceMultiplier;
+    }
+
+    public int getPeriodLimit() {
+        return periodLimit;
+    }
+
+    @Override
+    public String toString() {
+        return "ReportRulesDto{" +
+                "topPerformersThreshold=" + topPerformersThreshold +
+                ", useExperienceMultiplier=" + useExperienceMultiplier +
+                ", periodLimit=" + periodLimit +
+                '}';
+    }
+}
