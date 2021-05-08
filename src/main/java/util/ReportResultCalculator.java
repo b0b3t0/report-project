c66ba17ed270fb28b40ed654733bba7fbeb9049a
b0b3t0 <bnenov91@gmail.com>
2021-05-08 15:20:13 +0300

    Refactoring calculating of the score

--- src/main/java/util/ReportResultCalculator.java
+++ src/main/java/util/ReportResultCalculator.java
@@ -1,36 +1,43 @@
 package util;
 
+import data.SellerResult;
 import data.input.ReportRulesDto;
 import data.input.SellerDataDto;
-import data.SellerResult;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReportResultCalculator {
 
-    public static List<SellerResult> calcResult (SellerDataDto[] sellers, ReportRulesDto rules) {
+    public static List<SellerResult> calcResult(SellerDataDto[] sellers, ReportRulesDto rules) {
         List<SellerResult> resultList = new ArrayList<>();
 
         for (SellerDataDto seller : sellers) {
-            SellerResult result = new SellerResult(seller.getName());
-
             if (seller.getSalesPeriod() > rules.getPeriodLimit()) {
-                break;
+                continue;
             }
 
-            if (rules.isUseExperienceMultiplier()) {
-                result.setScore((double)seller.getTotalSales() / (seller.getSalesPeriod() * seller.getExperienceMultiplier()));
-            } else {
-                result.setScore((double)seller.getTotalSales() / seller.getSalesPeriod());
-            }
+            double score = calculateScore(rules, seller);
 
-            if (result.getScore() >= rules.getTopPerformersThreshold()) {
+
+            if (score >= rules.getTopPerformersThreshold()) {
+                SellerResult result = new SellerResult(seller.getName(), score);
                 resultList.add(result);
             }
 
         }
 
         return resultList;
+    }
+
+    private static double calculateScore(ReportRulesDto rules, SellerDataDto seller) {
+        double sellerTotalSales = seller.getTotalSales();
+        double sellerSalesPeriod = seller.getSalesPeriod();
+        
+        if (rules.isUseExperienceMultiplier()) {
+            return sellerTotalSales / sellerSalesPeriod * seller.getExperienceMultiplier();
+        } else {
+            return sellerTotalSales / sellerSalesPeriod;
+        }
     }
 }
