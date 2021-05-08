7670fa92c9a2081688626cc3acec4aac9df2283c
b0b3t0 <bnenov91@gmail.com>
2021-05-08 13:25:10 +0300

    Added static class for creating csv file, where we will writer our results

---
+++ src/main/java/util/FileCreator.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileCreator {
+
+    public static void createFile(String filePath) {
+
+        File file = new File(filePath);
+        boolean result;
+        try {
+            result = file.createNewFile();
+            if (result) {
+                System.out.println("File created " + file.getName() + "\n" + " at location: " + file.getCanonicalPath());
+            } else {
+                System.out.println("File " + file.getName() + " already exist at location: " + file.getCanonicalPath());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
