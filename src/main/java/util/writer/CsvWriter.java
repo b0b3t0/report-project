92e3d1412383f0ff6fe53224817bc2ceb6bda476
b0b3t0 <bnenov91@gmail.com>
2021-05-09 18:44:38 +0300

    Changed the name from Writer to CsvWrjter

--- src/main/java/util/writer/CsvWriter.java
+++ src/main/java/util/writer/CsvWriter.java
@@ -1,6 +1,7 @@
 package util.writer;
 
 import data.SellerResult;
+import util.reader.FileUtils;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -8,13 +9,28 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
-public class Writter {
+public class CsvWriter {
+
+    private static final String COLUMN_NAME = "Name";
+    private static final String COLUMN_SCORE = "Score";
+    private static final String SEPARATOR = ",";
+
 
     public static void writeResultIntoFile(File file, List<SellerResult> results) {
 
+        StringBuilder csvContent = new StringBuilder();
+        // add csv header
+        csvContent.append(COLUMN_NAME).append(SEPARATOR).append(COLUMN_SCORE);
+
+        for (SellerResult result : results) {
+            csvContent.append(result.getName()).append(SEPARATOR).append(result.getScore());
+        }
+
+        FileUtils.writeString(path, csvContent.toString());
+
+
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-            writer.write("Result");
-            writer.newLine();
+
             writer.write("Name  , Score");
             writer.newLine();
 
