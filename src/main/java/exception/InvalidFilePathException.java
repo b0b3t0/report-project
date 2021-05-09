3e7dcd4f7ca50c7225f02b4e0b65574ea30d884d
b0b3t0 <bnenov91@gmail.com>
2021-05-09 19:43:45 +0300

    Added directory for custom exceptions

---
+++ src/main/java/exception/InvalidFilePathException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class InvalidFilePathException extends RuntimeException {
+
+    private final String filePath;
+
+    public InvalidFilePathException(String filePath, Throwable cause) {
+        super("Could not read file at path " + filePath, cause);
+        this.filePath = filePath;
+    }
+}
