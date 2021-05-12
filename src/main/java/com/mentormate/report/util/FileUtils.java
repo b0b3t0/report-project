28b4931d8534260d1fe07b447224019cc326ba14
b0b3t0 <bnenov91@gmail.com>
2021-05-12 22:44:39 +0300

    Moved touchFile method in the class

--- src/main/java/com/mentormate/report/util/FileUtils.java
+++ src/main/java/com/mentormate/report/util/FileUtils.java
@@ -4,12 +4,15 @@ import com.google.gson.Gson;
 import com.mentormate.report.exception.InvalidFilePathException;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileUtils {
+
+    private static final Gson GSON = new Gson();
 
     private FileUtils() {
         // the utils class should not be instantiatable
@@ -18,22 +21,30 @@ public class FileUtils {
     public static <T> T readJson(String path, Class<T> clazz) throws InvalidFilePathException {
         String content;
         try {
-                content =  Files.readString(Path.of(path));
-            } catch (IOException e) {
-                // report failed file reading
-                throw new InvalidFilePathException(path, e);
-            }
+            content = Files.readString(Path.of(path));
+        } catch (IOException e) {
+            throw new InvalidFilePathException(path, e);
+        }
 
-        Gson gson = new Gson();
-
-        return gson.fromJson(content, clazz);
+        return GSON.fromJson(content, clazz);
     }
 
     public static void write(String path, String content) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
             writer.write(content);
         } catch (IOException e) {
-            throw new InvalidFilePathException(path, e.getCause());
+            throw new InvalidFilePathException(path, e);
+        }
+    }
+
+    public static File touchFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            file.createNewFile();
+
+            return file;
+        } catch (IOException e) {
+            throw new InvalidFilePathException(filePath, e);
         }
     }
 }
