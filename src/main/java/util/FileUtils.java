df004fdaeba863f42b2f8d0739882282f3876601
b0b3t0 <bnenov91@gmail.com>
2021-05-12 19:54:30 +0300

    Changed method throwed exception

---
+++ src/main/java/util/FileUtils.java
@@ -0,0 +1,41 @@
+package util;
+
+import com.google.gson.Gson;
+import exception.InvalidFilePathException;
+import exception.InvalidJsonFileException;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileUtils {
+
+    private FileUtils() {
+        // the utils class should not be instantiatable
+    }
+
+    public static <T> T readJson(String path, Class<T> clazz) throws InvalidFilePathException {
+        String content;
+        try {
+                content =  Files.readString(Path.of(path));
+            } catch (IOException e) {
+                // report failed file reading
+                throw new InvalidFilePathException(path, e);
+            }
+
+        Gson gson = new Gson();
+
+        return gson.fromJson(content, clazz);
+    }
+
+    public static void write(String path, String content) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            writer.write(content);
+        } catch (IOException e) {
+            throw new InvalidFilePathException(path, e.getCause());
+        }
+    }
+
+}
